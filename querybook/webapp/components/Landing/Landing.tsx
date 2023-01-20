b964b836924fd30cddb45e9ee3c936db2ce2b4d5
h-taneja <hemant.taneja@couture.ai>
2023-01-20 19:12:40 +0530

    landing description

--- querybook/webapp/components/Landing/Landing.tsx
+++ querybook/webapp/components/Landing/Landing.tsx
@@ -92,7 +92,16 @@ const DefaultLanding: React.FC = ({ children }) => {
                 <div className="Landing-subtitle">
                     Welcome back to Querybook
                 </div>
-            </div> */}
+            </div> */}<div>
+            <div className="Landing-greeting" style={{paddingBottom:"20px"}}>
+                    What is DBExplorer
+                </div>
+            <div className="Landing-subtitle" style={{paddingTop:"20px" ,paddingBottom:"20px"}}>
+            Easy-to-use interface that allows users to connect to various data sources and visualize the results of their queries in a variety of ways. One of the key features of Database Explorer is its collaboration capabilities, which allow multiple users to work on the same query or project, and share their work with others.                </div>
+                <div className="Landing-subtitle" style={{paddingTop:"20px" ,paddingBottom:"20px"}}>
+                One of the benefits of using Database Explorer is that it can help data teams work more efficiently. By providing a centralized platform for writing, running, and sharing SQL queries, Database Explorer can help streamline the process of working with data. Additionally, the collaboration features allow multiple team members to work on the same query or project at the same time, which can save time and reduce the risk of errors.
+                </div>
+                </div>
             <QuerybookSidebarUIGuide />
         </div>
     );
